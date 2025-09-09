37b6562cfea89320dfc85ec35228e242b385b24b
Yugasri Bandi <yugasribandi@gmail.com>
2025-09-09 20:47:51 +0530

    YUGASRIB0001 - Added AreaofCircle function

---
+++ src/main/java/com/example/javamavenjunithelloworld/Utility.py
@@ -0,0 +1,12 @@
+import math
+
+def area_of_circle(radius: float) -> float:
+    return math.pi * radius * radius
+
+def yuga():
+    print("I am Yuga")
+
+
+# Example usage
+print(area_of_circle(5))  # Example: Area of circle with radius 5
+yuga()
